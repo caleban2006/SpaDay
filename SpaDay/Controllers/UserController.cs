235a4b1fd349dfac27717c5e78b44ab9a3e27421
caleban2006 <logan.jen.2005@gmail.com>
2020-06-24 14:06:00 -0400

    Finished Studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (verify != newUser.Password)
+            {
+                ViewBag.error = "Passwords should match!";
+                ViewBag.name = newUser.Name;
+                ViewBag.email = newUser.Email;
+                return View("Add");
+            }
+            else
+            {
+                ViewBag.user = newUser;
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Id { get; }
+        private static int nextId = 1;
+
+        public User()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public User(string name, string email, string password) : this()
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is User user &&
+                   Id == user.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
